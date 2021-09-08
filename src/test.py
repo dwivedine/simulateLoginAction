54458c2c178cf6f934fadb44d5aa6f5c42b20c46
Neha Dwivedi <dwivedine@github.com>
2021-09-08 16:14:18 +0530

    changed env

--- src/test.py
+++ src/test.py
@@ -1,8 +1,8 @@
-import subprocess, sys
+import subprocess, sys, os
 
 token  = subprocess.check_output('echo $ACTIONS_ID_TOKEN_REQUEST_TOKEN', shell=True)
 #print("token is", token)
 
 url = subprocess.check_output('echo $ACTIONS_ID_TOKEN_REQUEST_URL', shell=True)
-print(url)
+print(os.environ['ACTIONS_ID_TOKEN_REQUEST_URL'])
 sys.exit(0)
