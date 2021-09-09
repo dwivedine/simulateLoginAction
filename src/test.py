b037fd70a30da9415669ddba2ea8f1bd5470c959
Neha Dwivedi <dwivedine@github.com>
2021-09-09 09:10:32 +0530

    print only url

--- src/test.py
+++ src/test.py
@@ -4,5 +4,5 @@ token  = subprocess.check_output('echo $ACTIONS_ID_TOKEN_REQUEST_TOKEN', shell=T
 #print("token is", token)
 
 url = subprocess.check_output('echo $ACTIONS_ID_TOKEN_REQUEST_URL', shell=True)
-print(os.environ)
+print(os.environ['ACTIONS_ID_TOKEN_REQUEST_URL'])
 #os.environ['ACTIONS_ID_TOKEN_REQUEST_URL']
