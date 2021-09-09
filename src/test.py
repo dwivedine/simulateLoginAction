4849e9b13bb6b6e3c9d95cab8ecab3474e1fcf6b
Neha Dwivedi <dwivedine@github.com>
2021-09-09 08:03:33 +0530

    corrected env var name

--- src/test.py
+++ src/test.py
@@ -4,5 +4,5 @@ token  = subprocess.check_output('echo $ACTIONS_ID_TOKEN_REQUEST_TOKEN', shell=T
 #print("token is", token)
 
 url = subprocess.check_output('echo $ACTIONS_ID_TOKEN_REQUEST_URL', shell=True)
-print(os.environ['ACTIONS_ID_TOKEN_REQUEST_URL'])
-sys.exit(0)
+print(os.getenv('ACTIONS_ID_TOKEN_REQUEST_URL'))
+#os.environ['ACTIONS_ID_TOKEN_REQUEST_URL']
